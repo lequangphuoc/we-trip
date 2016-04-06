65680f8c777c8d4eb77bed932ebea38e7bffaaf4
Lan Doan <lan@tinypulse.com>
2016-04-07 00:33:15 +0700

    first UI with login and register

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Shopping
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
       g.test_framework :rspec
