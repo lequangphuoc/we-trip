cb5e57175cf09c6a94418c849fcab9d3a1d26d07
Quan Vu <nongdenchet8435848@gmail.com>
2016-04-09 18:02:00 +0700

    refactor calculate points

--- spec/services/calculate_points_service_spec.rb
+++ spec/services/calculate_points_service_spec.rb
@@ -3,26 +3,33 @@ require 'rails_helper'
 RSpec.describe CalculatePointsService do
 	before(:each) do
 		@user = create(:user)
+    @service = CalculatePointsService.new(@user.id)
 	end
 
-	it "Adding points when viewed" do
-		CalculatePointsService.new(@user.id, "view")
-		expect(User.find(@user.id).point).to eq(1)
-	end
+	describe '#add_point' do
+    it 'Adding points when viewed' do
+      @service.add_point('view')
+      expect(User.find(@user.id).point).to eq(1)
+    end
 
+    it 'Adding points when cloned' do
+      @service.add_point('clone')
+      expect(User.find(@user.id).point).to eq(5)
+    end
 
-	it "Adding points when cloned" do
-		CalculatePointsService.new(@user.id, "clone")
-		expect(User.find(@user.id).point).to eq(5)
-	end
+    it 'Adding points when published' do
+      @service.add_point('published')
+      expect(User.find(@user.id).point).to eq(10)
+    end
 
-	it "Adding points when published" do
-		CalculatePointsService.new(@user.id, "published")
-		expect(User.find(@user.id).point).to eq(10)
-	end
+    it 'Adding points with empty type' do
+      @service.add_point('')
+      expect(User.find(@user.id).point).to eq(0)
+    end
 
-	it "Adding points with empty type" do
-		CalculatePointsService.new(@user.id, "")
-		expect(User.find(@user.id).point).to eq(0)
+    it 'Adding points with other type' do
+      @service.add_point('xxx')
+      expect(User.find(@user.id).point).to eq(0)
+    end
 	end
 end
