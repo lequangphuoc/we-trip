23aed6d0d5081ab489eb7f51aacc059e0aaecf2c
Corsad <s3372771@rmit.edu.vn>
2016-04-09 19:30:27 +0700

    Remove Place and Region template

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,15 +8,7 @@ class LocationsController < ApplicationController
 
 	private
 	def init
-		@location = Region.where(id: params[:id]) + Place.where(id: params[:id])
-		case @location.first.class.name
-		when 'Place'
-			@location = Place.find(params[:id])
-			@photos = PlacePhoto.where(place: @location)
-		when 'Region'
-			@location = Region.find(params[:id])
-		else
-			redirect_to root_path
-		end
+		@location = Place.find(params[:id])
+		@photos = PlacePhoto.where(place: @location)
 	end
 end
