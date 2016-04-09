feddd7373645a839c94c79a3cc3a0c40d8041995
Corsad <s3372771@rmit.edu.vn>
2016-04-10 00:39:27 +0700

    Add Test for Decorator

--- spec/decorators/place_decorator_spec.rb
+++ spec/decorators/place_decorator_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe PlaceDecorator do
+	let(:place) { create(:place).decorate}
+
+	it "Get Map Url" do
+		exptectedString = "https://www.google.com/maps/embed/v1/place?q=place_id:test1&key=AIzaSyBrJ4kgKukKBQGXsu7ULAynrPaBz65k3iA"
+		expect(place.map_url).to eq(exptectedString)
+	end
 end
--- spec/decorators/place_photo_decorator_spec.rb
+++ spec/decorators/place_photo_decorator_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
 describe PlacePhotoDecorator do
+	before(:each) do
+		@place = create(:place).decorate
+		@place_photo = create(:place_photo).decorate
+		@place_photo.place_id = @place.id
+	end
+
+	it "Get Place Name" do
+		expect(@place_photo.place_name).to eq("Tra Sua")
+	end
+
+	it "Get url" do
+		expectedString = "https://maps.googleapis.com/maps/api/place/photo?maxheight=720&photoreference=test1&key=AIzaSyBrJ4kgKukKBQGXsu7ULAynrPaBz65k3iA"
+		expect(@place_photo.place_name).to eq("Tra Sua")
+	end
 end
