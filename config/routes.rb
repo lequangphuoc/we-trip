7d857a55c4b3433b6f4b90399abe0d2f6b110bff
Quan Vu <nongdenchet8435848@gmail.com>
2016-04-10 13:28:56 +0700

    fix routes path

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,12 @@ Rails.application.routes.draw do
 
   get 'auth/:provider/callback' => 'sessions#callback'
   resource :sessions, only: [:create, :destroy]
+
+  resources :friend_relations, only: [:create]
+  post 'friend_relations/confirm' => 'friend_relations#confirm'
+  post 'friend_relations/reject' => 'friend_relations#reject'
+
   resources :users, only: [:create, :edit, :update]
-  resources :friend_relations, only: [:create] do
-    new do
-      post :confirm
-      post :reject
-    end
-  end
   resources :places, only: [:show, :index]
   resources :trips
 end
