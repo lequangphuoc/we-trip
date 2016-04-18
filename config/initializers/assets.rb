277627ed5c622ebbdee5388192b0ddde9a79ddec
Quan Vu <nongdenchet8435848@gmail.com>
2016-04-18 23:26:21 +0700

    fix asset compile custom_marker.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application_1.scss, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+# Rails.application.config.assets.precompile += %w( custom_marker.js )
