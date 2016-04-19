7e49ed4e0670f0faf1a1c1e51998581a841ff90e
Quan Vu <nongdenchet8435848@gmail.com>
2016-04-19 07:22:51 +0700

    add infowondow to map

--- app/assets/javascripts/custom_marker.js
+++ app/assets/javascripts/custom_marker.js
@@ -1,34 +1,44 @@
-function CustomMarker(latlng, map, url, args) {
+function CustomMarker(latlng, map, url, title, args) {
     this.latlng = latlng;
-    this.args = args;
     this.url = url;
+    this.title = title;
+    this.map = map;
+    this.args = args;
     this.setMap(map);
 }
-
-// <%= image_tag(attraction.image_url, height: '50', width: '50', style: 'vertical-align: bottom', class: 'img-circle') %>
 
 CustomMarker.prototype = new google.maps.OverlayView();
 CustomMarker.prototype.draw = function () {
     var self = this;
     var root = this.root;
 
+    // styling
     if (!root) {
         root = this.root = document.createElement('img');
         root.className = 'img-circle';
         root.style.position = 'absolute';
         root.style.cursor = 'pointer';
         root.style.border = '3px solid red';
-        root.width = '56';
-        root.height = '56';
+        root.width = '50';
+        root.height = '50';
         root.src = encodeURI(this.url.replace(/&amp;/g, '&'));
         var panes = this.getPanes();
         panes.overlayImage.appendChild(root);
     }
 
+    // add infowondow
+    var infowindow = new google.maps.InfoWindow({
+        content: "<h4>" + this.title + "<h4/>"
+    });
+    google.maps.event.addDomListener(root, 'click', function() {
+        infowindow.open(self.map, self);
+    });
+
+    // add position
     var point = this.getProjection().fromLatLngToDivPixel(this.latlng);
     if (point) {
-        root.style.left = (point.x) + 'px';
-        root.style.top = (point.y) + 'px';
+        root.style.left = (point.x - 25) + 'px';
+        root.style.top = (point.y - 25) + 'px';
     }
 };
 
