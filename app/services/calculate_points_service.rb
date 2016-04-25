f32b35917a256d9489d73e2243d5543e70c880b5
Corsad <s3372771@rmit.edu.vn>
2016-04-25 22:43:24 +0700

    User.transaction

--- app/services/calculate_points_service.rb
+++ app/services/calculate_points_service.rb
@@ -4,23 +4,26 @@ class CalculatePointsService
   end
 
   def add_point_by_trip (type)
-    @trip.users.each do |user|
-      add_point_by_user(user, type)
+
+    User.transaction do
+      @trip.users.each do |user|
+        add_point_by_user(user, type)
+      end
     end
   end
 
   def add_point_by_user(user ,type)
     case type
-      when 'view'
-        user.increment!(:point)
-      when 'create'
-        user.increment!(:point, by = 3)
-      when 'clone'
-        user.increment!(:point, by = 5)
-      when 'published'
-        user.increment!(:point, by = 10)
-      else
-        return
+    when 'view'
+      user.increment!(:point)
+    when 'create'
+      user.increment!(:point, by = 3)
+    when 'clone'
+      user.increment!(:point, by = 5)
+    when 'published'
+      user.increment!(:point, by = 10)
+    else
+      return
     end
   end
 end
