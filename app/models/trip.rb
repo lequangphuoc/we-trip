609fae0c2ae93e408b7ee35ba286828a698cb029
Corsad <s3372771@rmit.edu.vn>
2016-04-25 20:58:32 +0700

    Missing destination

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -68,8 +68,9 @@ class Trip < ActiveRecord::Base
   def self.search_trips(departure, destination)
     @departure = Region.find_by(name: departure)
     @destination = Region.find_by(name: destination)
-
-    if @departure != nil && @destination != nil
+    if @departure != nil && destination == nil
+      Trip.where(departure_id: @departure.id)
+    elsif @departure != nil && @destination != nil
       @attractions = Attraction.where(place_id: Place.where(region_id: Region.where(id: @destination.id).pluck(:id)).pluck(:id))
 
       Trip.where(id: ScheduleDay.where(id: @attractions.collect(&:schedule_day_id)).pluck(:trip_id).uniq).where(departure_id: @departure.id)
