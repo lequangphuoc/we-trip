42fcedf79347a3989edc321fa2e2c7d6bb478432
Quan Vu <nongdenchet8435848@gmail.com>
2016-04-27 01:25:37 +0700

    finish app

--- app/services/get_album_service.rb
+++ app/services/get_album_service.rb
@@ -5,14 +5,18 @@ class GetAlbumService
 
   def execute
     # graph = Koala::Facebook::API.new(@provider.access_token)
-    graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
-    graph.get_connections('me', 'albums').map { |album|
-      {
-          id: album['id'],
-          name: album['name'],
-          count: album['count'],
-          created_time: Time.new(album['created_time']).strftime('%d/%m/%Y')
+    begin
+      graph = Koala::Facebook::API.new(ENV['ACCESS_TOKEN'])
+      graph.get_connections('me', 'albums').map { |album|
+        {
+            id: album['id'],
+            name: album['name'],
+            count: album['count'],
+            created_time: Time.new(album['created_time']).strftime('%d/%m/%Y')
+        }
       }
-    }
+    rescue Exception => e
+      []
+    end
   end
 end
