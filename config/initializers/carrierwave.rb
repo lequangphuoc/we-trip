b839c927ed52a73b254bb47084db28c625a560e4
Quan Vu <nongdenchet8435848@gmail.com>
2016-04-26 20:52:42 +0700

    fix ENV tests

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,30 +1,32 @@
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV['S3_BUCKET_NAME']
-  config.aws_acl    = 'public-read'
+  if Rails.env.test?
+    config.storage    = :aws
+    config.aws_bucket = ENV['S3_BUCKET_NAME']
+    config.aws_acl    = 'public-read'
 
-  # Optionally define an asset host for configurations that are fronted by a
-  # content host, such as CloudFront.
-  config.asset_host = ENV['S3_ASSET_URL']
+    # Optionally define an asset host for configurations that are fronted by a
+    # content host, such as CloudFront.
+    config.asset_host = ENV['S3_ASSET_URL']
 
-  # The maximum period for authenticated_urls is only 7 days.
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    # The maximum period for authenticated_urls is only 7 days.
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
 
-  # Set custom options such as cache control to leverage browser caching
-  config.aws_attributes = {
-      expires: 1.week.from_now.httpdate,
-      cache_control: 'max-age=604800'
-  }
+    # Set custom options such as cache control to leverage browser caching
+    config.aws_attributes = {
+        expires: 1.week.from_now.httpdate,
+        cache_control: 'max-age=604800'
+    }
 
-  def download_url(filename)
-    url(response_content_disposition: %Q{attachment; filename="#{filename}"})
+    def download_url(filename)
+      url(response_content_disposition: %Q{attachment; filename="#{filename}"})
+    end
+
+    config.aws_credentials = {
+        access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        region:            ENV.fetch('AWS_REGION') # Required
+    }
   end
-
-  config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION') # Required
-  }
 
   # Optional: Signing of download urls, e.g. for serving private
   # content through CloudFront.
