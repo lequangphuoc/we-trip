195f984f63bf9ad038c8eaf8f2b3b64cf98bd137
Quan Vu <nongdenchet8435848@gmail.com>
2016-04-29 23:23:44 +0700

    improve facebook request

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,7 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
-           :scope => 'email,user_about_me,user_friends,user_photos',
+           :image_size => {width: 200, height: 200},
+           :scope => 'email,user_friends,user_photos',
            :display => 'popup'
 end
